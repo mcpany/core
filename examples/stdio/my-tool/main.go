222e8dc7e7b77d611f1dae8ead942399b32ae19b
Kevin Wang <kevixw@gmail.com>
2025-12-02 00:35:19 +0000

    feat: Add stdio example and test
    This change adds a new example for the stdio service type, including a self-contained Go program, configuration, and an end-to-end test. It also removes the `jq` dependency from the test suite, making it more robust and reliable for CI/CD pipelines.

---
+++ examples/stdio/my-tool/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Request struct {
+	Name string `json:"name"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	var req Request
+	if err := json.Unmarshal(input, &req); err != nil {
+		fmt.Fprintf(os.Stderr, "Error unmarshalling JSON: %v\n", err)
+		os.Exit(1)
+	}
+
+	resp := Response{
+		Message: fmt.Sprintf("Hello, %s!", req.Name),
+	}
+
+	if err := json.NewEncoder(os.Stdout).Encode(resp); err != nil {
+		fmt.Fprintf(os.Stderr, "Error marshalling JSON: %v\n", err)
+		os.Exit(1)
+	}
+}
