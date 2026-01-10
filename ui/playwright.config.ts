550a1537154b2fa09e351244471933ca3a7ce01a
Kevin Wang <kevixw@gmail.com>
2026-01-10 19:59:09 +0000

    feat(ui): enable parallel testing and concurrency test

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -4,6 +4,7 @@
  */
 
 import { defineConfig, devices } from '@playwright/test';
+import os from 'os';
 
 export default defineConfig({
   testDir: './tests',
@@ -12,7 +13,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: os.cpus().length,
   outputDir: 'test-results/artifacts',
   reporter: [['line'], ['html', { outputFolder: 'playwright-report/html' }], ['json', { outputFile: 'test-results/test-results.json' }]],
   timeout: 60000,
