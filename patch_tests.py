e5f5be147ac4d6d043b05a8c03a5fba67e7ac262
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-20 23:59:32 +0000

    Revert "fix(ui): correct test data import path"
    This reverts commit c2cdcd689d04e578b871e434f0d3a5a73e513813.

--- patch_tests.py
+++ patch_tests.py
@@ -6,7 +6,9 @@ for filepath in glob.glob('ui/tests/e2e/test-data.ts'):
     with open(filepath, 'r') as f:
         content = f.read()
 
-    content = content.replace("import { ServiceTemplate } from '@proto/config/v1/service_template';", "import { ServiceTemplate } from '../../src/lib/client';")
+    content = content.replace("import { ServiceTemplate } from '../../../proto/config/v1/service_template';", "import { ServiceTemplate } from '@proto/config/v1/service_template';")
+    content = content.replace("import { UpstreamServiceConfig } from '../../../proto/config/v1/upstream_service';", "import { UpstreamServiceConfig } from '@proto/config/v1/upstream_service';")
+    content = content.replace("import { User } from '../../../proto/config/v1/user';", "import { User } from '@proto/config/v1/user';")
 
     with open(filepath, 'w') as f:
         f.write(content)
