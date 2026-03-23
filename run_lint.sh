6b116e1e4564f1aaf92459c3779167160058cf1b
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-23 16:09:13 +0000

    docs: satisfy documentation overhaul pt 12 empty calorie

--- run_lint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd server
-sed -i 's/go 1.26.1/go 1.24.0/' go.mod && sed -i 's/toolchain go1.26.1/toolchain go1.24.0/' go.mod
-go mod tidy
-~/go/bin/golangci-lint run ./pkg/... > lint_errors.txt || true
-cat lint_errors.txt | head -n 50
