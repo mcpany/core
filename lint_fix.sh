4a09257b06d1676085dca3ffb2ea4435b362bc5e
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-23 20:01:28 +0000

    Resolve all linting errors from changes

--- lint_fix.sh
+++
@@ -1,4 +0,0 @@
-# Remove the unused variable in public_api tests if we missed any
-cd server
-$(go env GOPATH)/bin/golangci-lint run ./... > lint.log 2>&1
-cat lint.log | grep -v 'could not import' | grep -v 'unsupported version: 2'
