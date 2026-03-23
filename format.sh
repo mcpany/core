f4fdf2d2eee44da3a1f036c02562dc51bd6674e7
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-23 22:13:09 +0000

    fix: restore CI checks and fix bazel build dependencies

--- format.sh
+++ format.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+# Copyright 2026 Author(s) of MCP Any
+# SPDX-License-Identifier: Apache-2.0
+
 for file in $(find server/pkg -name "*.go" | grep -v "_test.go"); do
-    gofmt -w $file
+    gofmt -w "$file"
 done
--- update_gazelle.sh
+++ update_gazelle.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Copyright 2026 Author(s) of MCP Any
+# SPDX-License-Identifier: Apache-2.0
+
 set -e
 bazel run //:gazelle -- update-repos -from_file=go.mod -to_macro=repositories.bzl%go_repositories
