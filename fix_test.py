11c53c0a4f274bc497b695030dff972b038a8821
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-24 08:50:56 +0000

    Fix CI lint failures in store_load_test.go
    - Fix unused variable test issue using `_ = store`.

--- fix_test.py
+++ fix_test.py
@@ -3,7 +3,7 @@ import re
 with open("server/pkg/storage/postgres/store_load_test.go", "r") as f:
     content = f.read()
 
-content = content.replace('\n\t\t_ = store\n', '\n')
+content = content.replace("func TestStore_Load(t *testing.T) {\n\t// Use MatchExpectationsInOrder(false) because go routines can query in any order\n\t// Actually no, just simple mock with MatchAnyArgs will do, but here order is generic", "func TestStore_Load(t *testing.T) {")
 
 with open("server/pkg/storage/postgres/store_load_test.go", "w") as f:
     f.write(content)
