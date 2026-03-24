7f927a468a74385154c162ca609cd36dd612d1d6
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-24 00:33:08 +0000

    fix: re-run clean lints and remove unwanted formatter runs

--- patch_tools.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = 'ui/src/tests/tools.spec.ts';
-if (fs.existsSync(file)) {
-  let code = fs.readFileSync(file, 'utf8');
-  code = code.replace(/ws\.send\(data\)/g, "ws.send(data)");
-  fs.writeFileSync(file, code);
-}
