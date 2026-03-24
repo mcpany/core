f6c31fa145f4db37c628a5df73d03dc1b510cbff
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-24 05:31:23 +0000

    fix: resolve bazel build and circleci lint issues

--- update_gazelle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright 2026 Author(s) of MCP Any
-# SPDX-License-Identifier: Apache-2.0
-
-set -e
-bazel run //:gazelle -- update-repos -from_file=go.mod -to_macro=repositories.bzl%go_repositories
