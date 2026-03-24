e823eaf3eefaa7f15dd69e022ead6c20952bf274
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-24 06:36:39 +0000

    chore: prevent bash -e exit on linux oom killed lint tasks

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -34,11 +34,17 @@ if [ -z "$GOLANGCI_LINT_BIN" ]; then
 fi
 
 export GOGC=off
+
+# Prevent bazel bash runner from exiting on pipeline errors
+set +e
+
 # Break up golangci-lint into multiple runs to prevent OOM
-"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/cmd/... || true
-"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/pkg/... || true
-"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/tests/... || true
-"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/examples/... || true
+"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/cmd/...
+"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/pkg/...
+"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/tests/...
+"$GOLANGCI_LINT_BIN" run --timeout 30m --concurrency 1 --fix ./server/examples/...
+
+set -e
 
 echo "Running pre-commit..."
 if command -v pre-commit >/dev/null 2>&1; then
