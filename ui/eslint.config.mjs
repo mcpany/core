253304e37b2f3e838306b858de569d90919a6ab3
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-25 11:01:52 +0000

    Final eslint config fix

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -21,6 +21,7 @@ export default [
       "@typescript-eslint": tsPlugin,
     },
     rules: {
+       ...tsPlugin.configs.recommended.rules,
        "no-undef": "off",
        "@typescript-eslint/no-unused-vars": "off",
        "@typescript-eslint/no-explicit-any": "off"
