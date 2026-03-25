65d52e71c941378333c5e0e642543ca4bb4866eb
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-25 04:51:50 +0000

    fix(ui): Explicitly omit typescript recommended rule bundle
    Fixes lint errors triggered by removing the typescript-eslint recommended bundle.

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -23,7 +23,8 @@ export default [
     rules: {
        "no-undef": "off",
        "@typescript-eslint/no-unused-vars": "off",
-       "@typescript-eslint/no-explicit-any": "off"
+       "@typescript-eslint/no-explicit-any": "off",
+       "@typescript-eslint/ban-ts-comment": "off"
     }
   },
   {
