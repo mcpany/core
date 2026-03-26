a6a47ace18b85619e69abb96fc256fca67ce0c60
ql-owo-lp <678729+ql-owo-lp@users.noreply.github.com>
2026-03-26 02:06:38 +0000

    trigger build

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -1,3 +1,6 @@
+import next from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
+import nextTypescript from "eslint-config-next/typescript.js";
 /**
  * Copyright 2026 Author(s) of MCP Any
  * SPDX-License-Identifier: Apache-2.0
@@ -7,30 +10,27 @@ import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import nextPlugin from "@next/eslint-plugin-next";
 
-export default [
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaFeatures: { modules: true, jsx: true },
-        sourceType: "module",
-        ecmaVersion: "latest"
-      }
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      "@next/next": nextPlugin
-    },
-    rules: {
-       ...tsPlugin.configs.recommended.rules,
-       ...nextPlugin.configs.recommended.rules,
-       "no-undef": "off",
-       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
-       "@typescript-eslint/no-explicit-any": "warn"
+export default [...next, ...nextCoreWebVitals, ...nextTypescript, {
+  files: ["**/*.ts", "**/*.tsx"],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaFeatures: { modules: true, jsx: true },
+      sourceType: "module",
+      ecmaVersion: "latest"
     }
   },
-  {
-    ignores: [".next/**", "node_modules/**", "eslint.config.mjs", "next-env.d.ts"]
+  plugins: {
+    "@typescript-eslint": tsPlugin,
+    "@next/next": nextPlugin
+  },
+  rules: {
+     ...tsPlugin.configs.recommended.rules,
+     ...nextPlugin.configs.recommended.rules,
+     "no-undef": "off",
+     "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
+     "@typescript-eslint/no-explicit-any": "warn"
   }
-];
+}, {
+  ignores: [".next/**", "node_modules/**", "eslint.config.mjs", "next-env.d.ts"]
+}];
